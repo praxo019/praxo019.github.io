151552f47920aab2b19cb309b03e00e3e2fca24a
praxo019 <84620007+praxo019@users.noreply.github.com>
2021-11-11 11:36:17 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "26 Nov 2021 20:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
